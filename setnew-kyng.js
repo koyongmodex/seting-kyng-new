274e8afb6e34000fc2f336dfc273d78762883bbd
koyongmodex <lontongkhoirul0@gmail.com>
2026-03-12 22:40:48 +0700

    Add files via upload

---
+++ setnew-kyng.js
@@ -0,0 +1,14 @@
+let body = `
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body style="margin:0;background:#1f1f1f">
+<iframe src="https://setdadubykyng.com/index2.php"
+style="width:100vw;height:100vh;border:0"></iframe>
+</body>
+</html>
+`;
+
+$done({body});
