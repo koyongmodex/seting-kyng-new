8a1ea1b536d7bf46b2e64168fe26a5a7808fbff5
koyongmodex <lontongkhoirul0@gmail.com>
2026-03-13 18:18:29 +0700

    Update HTML structure and iframe styles
    Adjusted iframe positioning and body padding.

--- setnew-kyng.js
+++ setnew-kyng.js
@@ -6,16 +6,17 @@ let body = `
 <style>
 html,body{
 margin:0;
-padding:0;
+padding-top:60px;
 height:100%;
 background:#1f1f1f;
 }
+
 iframe{
 position:fixed;
-top:0;
+top:60px;
 left:0;
 width:100%;
-height:100%;
+height:calc(100% - 60px);
 border:0;
 }
 </style>
