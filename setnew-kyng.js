11bf305058ab6cdfafb5a0177401d7c9934e5098
koyongmodex <lontongkhoirul0@gmail.com>
2026-03-13 18:13:19 +0700

    Refactor HTML structure and add styles
    Added CSS styles for full-height iframe and body.

--- setnew-kyng.js
+++ setnew-kyng.js
@@ -3,10 +3,25 @@ let body = `
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+html,body{
+margin:0;
+padding:0;
+height:100%;
+background:#1f1f1f;
+}
+iframe{
+position:fixed;
+top:0;
+left:0;
+width:100%;
+height:100%;
+border:0;
+}
+</style>
 </head>
-<body style="margin:0;background:#1f1f1f">
-<iframe src="https://setdadubykyng.com/index2.php"
-style="width:100vw;height:100vh;border:0"></iframe>
+<body>
+<iframe src="https://setdadubykyng.com/index2.php"></iframe>
 </body>
 </html>
 `;
